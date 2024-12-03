dfbcb9c862c984caa1225b7cf505aada5f8f0a88
Mike McGowan <mike.mcgowan@getbridge.com>
2024-12-03 11:35:11 +0000

    Simplify days in month calculation

--- make-markdown-month.lua
+++ make-markdown-month.lua
@@ -1,19 +1,18 @@
 local lib = require("lib")
 
-local function month_info(year, month)
-  local first_day_timestamp = os.time({ year = year, month = month, day = 1, hour = 0, min = 0, sec = 0 })
-  local first_day_of_week = os.date("*t", first_day_timestamp).wday -- 1=Sunday, 2=Monday, ..., 7=Saturday
-  month = month + 1
-  if month > 12 then
-    month = 1
-    year = year + 1
+local DAYS_IN_MONTH = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
+
+local function get_days_in_month(year, month)
+  if month == 2 and year % 4 == 0 and (year % 100 ~= 0 or year % 400 == 0) then
+    return 29
+  else
+    return DAYS_IN_MONTH[month]
   end
-  local last_day_timestamp = os.time({ year = year, month = month, day = 1, hour = 0, min = 0, sec = 0 }) - 1
-  local last_day = os.date("*t", last_day_timestamp).day
-  return {
-    days_in_month = last_day,
-    first_day_of_week = first_day_of_week,
-  }
+end
+
+local function get_first_day_of_week(year, month)
+  local first_day_timestamp = os.time({ year = year, month = month, day = 1, hour = 0, min = 0, sec = 0 })
+  return os.date("*t", first_day_timestamp).wday -- 1=Sunday, 2=Monday, ..., 7=Saturday
 end
 
 local function day_of_week_text(day_of_week)
@@ -33,6 +32,9 @@ if not arg[2] then
   print(("Missing arg 2/2: month"):add_colour(lib.colours.yellow))
 end
 
+arg[1] = tonumber(arg[1])
+arg[2] = tonumber(arg[2])
+
 if not arg[1] or not arg[2] then
   os.exit(1)
 end
@@ -40,9 +42,9 @@ end
 print("| Day | Alcohol | Coffee |")
 print("| --- | ------- | ------ |")
 
-local info = month_info(arg[1], arg[2])
-local day_of_week = info.first_day_of_week
-for day_in_month = 1, info.days_in_month do
+local days_in_month = get_days_in_month(arg[1], arg[2])
+local day_of_week = get_first_day_of_week(arg[1], arg[2])
+for day_in_month = 1, days_in_month do
   print("|  " .. string.format("%2d", day_in_month) .. " | " .. day_of_week_text(day_of_week) .. "     |        |")
   day_of_week = day_of_week + 1
   if day_of_week > 7 then
